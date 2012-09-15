01ed5ae6e8a05ee0c0bddbc982305d887051fec0
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2012-09-15 14:37:08 -0600

    Add update admin command

--- randblog/admin.py
+++ randblog/admin.py
@@ -6,4 +6,17 @@ if __name__ == '__main__':
         from randblog.rss.feed import Feed
         if sys.argv[2] == 'add':
             Feed.add(sys.argv[3], sys.argv[4])
+        if sys.argv[2] == 'update':
+            if len(sys.argv) > 3 and sys.argv[3] == 'False':
+                update = False
+            else:
+                update = True
 
+            from gevent.pool import Pool
+            p = Pool()
+            feeds = Feed.find()
+            for res in p.map(lambda f: f.update(), feeds):
+                if not res is None:
+                    res.join()
+                    print res.value
+ 
