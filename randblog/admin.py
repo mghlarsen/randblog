784d7a9827e3407f513de1841d1391a0dd48631a
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2012-09-27 10:35:02 -0600

    Add Generator

--- randblog/admin.py
+++ randblog/admin.py
@@ -1,6 +1,7 @@
 import sys
 from randblog.rss.feed import Feed
 from gevent.pool import Pool
+import random
 pool = Pool()
 
 def do_task_for_feeds(task, feeds):
@@ -8,6 +9,40 @@ def do_task_for_feeds(task, feeds):
         if not res is None and not isinstance(res, dict):
             res.join()
             print res.value
+
+def pickWord(stats, prevWords):
+    s = stats
+    for w in prevWords:
+        s = s[w]
+    choices = []
+    for word, count in s.items():
+        choices += [word] * count
+    return random.choice(choices)
+
+def pickWordMulti(stats, prevWords, maxN):
+    choices = []
+    wChoices = []
+    for n in range(2, maxN+1):
+        s = stats[str(n) + 'gram']
+        found = True
+        for w in prevWords[maxN - n:]:
+            if not w in s:
+                found = False
+                break
+            s = s[w]
+        if found:
+            choices.append((n, s))
+
+    for n, s in choices:
+        for word, count in s.items():
+            wChoices += ([word] * (count * (n - 1)**4))
+        if n - 2 == len(choices) and n != maxN:
+            for word, count in s.items():
+                wChoices += ([word] * (count * maxN**4))
+
+    if len(wChoices) == 0:
+        return '<END>'
+    return random.choice(wChoices)
 
 if __name__ == '__main__':
     print sys.argv[1:]
@@ -43,4 +78,33 @@ if __name__ == '__main__':
             with open(sys.argv[3], 'w') as out:
                 import json
                 out.write(json.dumps(Feed.stats()))
+        elif sys.argv[2] == 'generate':
+            stats = Feed.stats()
+            n = None
 
+            if len(sys.argv) > 3:
+                n = int(sys.argv[3])
+            
+            if not n is None:
+                nStats = stats[str(n) + 'gram']
+                words = ['<START>'] * (n - 1)
+    
+                curr = pickWord(nStats, words)
+
+                while curr != '<END>':
+                    words.append(curr)
+                    curr = pickWord(nStats, words[len(words) - (n - 1):])
+
+                words = words[n-1:]
+
+            else:
+                words = ['<START>'] * 4
+
+                curr = pickWordMulti(stats, words, 5)
+                while curr != '<END>':
+                    words.append(curr)
+                    curr = pickWordMulti(stats, words[len(words) - 4:], 5)
+
+                words = words[4:]
+
+            print ' '.join(map(lambda s: s.replace('<period>', '.').replace('<dollar>', '$'), words))
