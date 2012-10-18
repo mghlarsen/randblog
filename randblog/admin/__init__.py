8f8b6696e414da507d8707689d28f3fe7d370893
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2012-10-18 11:05:05 -0600

    Factor out command implementations into separate functions

--- randblog/admin/__init__.py
+++ randblog/admin/__init__.py
@@ -1,8 +1,116 @@
-import sys
 from randblog.rss.feed import Feed
 from gevent.pool import Pool
 import random
 pool = Pool()
+
+def feed_command(args):
+    if args[0] == 'add':
+        feed_add(args[1:])
+    elif args[0] == 'update':
+        feed_update(args[1:])
+    elif args[0] == 'clean_actions':
+        feed_clean_actions(args[1:])
+
+def feed_add(args):
+    Feed.add(args[0], args[1])
+
+def feed_update(args):
+    if len(args) > 0 and args[0] == 'False':
+        update = False
+    else:
+        update = True
+
+    feeds = Feed.find()
+    do_task_for_feeds(Feed.update, feeds)
+
+def feed_clean_actions(args):
+    if args[0] == 'show':
+        feed_clean_actions_show(args[1:])
+    elif args[0] == 'add':
+        feed_clean_actions_add(args[1:])
+
+def feed_clean_actions_show(args):
+    if len(args) > 0:
+        feeds = [Feed.get(args[0])]
+    else:
+        feeds = Feed.find()
+
+    for feed in feeds:
+        print feed.name
+        if 'clean_actions' in feed.info:
+            for action in feed.info['clean_actions']:
+                print ' -', action['task'], action['selector']
+
+def feed_clean_actions_add(args):
+    f = Feed.get(args[0])
+    if not 'clean_actions' in f.info:
+        f.info['clean_actions'] = []
+    f.info['clean_actions'].append({'selector': args[1], 'task':args[2]})
+    f.save()
+
+def entry_command(args):
+    if args[0] == 'clean':
+        entry_clean(args[1:])
+    elif args[0] == 'stats':
+        entry_stats(args[1:])
+
+def entry_clean(args):
+    if len(args) > 0:
+        toClean = [Feed.get(f) for f in args]
+    else:
+        toClean = Feed.find()
+    do_task_for_feeds(Feed.clean, toClean)
+
+def entry_stats(args):
+    if len(args) > 0:
+        toStat = [Feed.get(f) for f in args]
+    else:
+        toStat = Feed.find()
+    do_task_for_feeds(Feed.stats_collect, toStat)
+
+#TODO: Split up into better names
+def global_command(args):
+    if args[0] == 'stats':
+        global_stats(args[1:])
+    elif args[0] == 'generate':
+        global_generate(args[1:])
+
+def global_stats(args):
+    stats = Feed.stats()
+    if len(args) > 0:
+        with open(args[0], 'w') as out:
+            import json
+            json.dump(stats, out)
+
+def global_generate(args):
+    stats = Feed.stats(False)
+    n = None
+
+    if len(args) > 0:
+        n = int(args[0])
+
+    if not n is None:
+        nStats = stats[str(n) + 'gram']
+        words = ['<START>'] * (n - 1)
+
+        curr = pickWork(nStats, words)
+
+        while curr != '<END>':
+            words.append(curr)
+            curr = pickWord(nStats, words[len(words) - (n - 1):])
+
+        words = words[n-1:]
+    else:
+        words = ['<START>'] * 4
+
+        curr = pickWordMulti(stats, words, 5)
+        while curr != '<END>':
+            words.append(curr)
+            curr = pickWordMulti(stats, words[len(words) - 4:], 5)
+
+        words = words[4:]
+
+    print ' '.join(map(lambda s: s.replace('<period>', '.').replace('<dollar>', '$'), words))
 
 def do_task_for_feeds(task, feeds):
     for res in pool.map(task, feeds):
@@ -43,87 +151,4 @@ def pickWordMulti(stats, prevWords, maxN):
         return '<END>'
     return random.choice(wChoices)
 
-if __name__ == '__main__':
-    print sys.argv[1:]
-    if sys.argv[1] == 'feed':
-        if sys.argv[2] == 'add':
-            Feed.add(sys.argv[3], sys.argv[4])
-        elif sys.argv[2] == 'update':
-            if len(sys.argv) > 3 and sys.argv[3] == 'False':
-                update = False
-            else:
-                update = True
 
-            feeds = Feed.find()
-            do_task_for_feeds(lambda f: f.update(), feeds)
-        elif sys.argv[2] == 'clean_actions':
-            if sys.argv[3] == 'show':
-                if len(sys.argv) > 4:
-                    feeds = [Feed.get(sys.argv[4])]
-                else:
-                    feeds = Feed.find()
-                for feed in feeds:
-                    print feed.name
-                    if 'clean_actions' in feed.info:
-                        for action in feed.info['clean_actions']:
-                            print ' -', action['task'], action['selector']
-
-            elif sys.argv[3] == 'add':
-                f = Feed.get(sys.argv[4])
-                if not 'clean_actions' in f.info:
-                    f.info['clean_actions'] = []
-                f.info['clean_actions'].append({'selector': sys.argv[5], 'task': sys.argv[6]})
-                f.save()
-    elif sys.argv[1] == 'entry':
-        if sys.argv[2] == 'clean':
-            if len(sys.argv) == 3:
-                toClean = Feed.find()
-            else:
-                toClean = [Feed.get(f) for f in sys.argv[3:]]
-            
-            do_task_for_feeds(lambda f: f.clean(), toClean)
-
-        elif sys.argv[2] == 'stats':
-            if len(sys.argv) == 3:
-                toStat = Feed.find()
-            else:
-                toStat = [Feed.get(f) for f in sys.argv[3:]]
-
-            do_task_for_feeds(lambda f: f.stats_collect(True), toStat)
-    elif sys.argv[1] == 'global':
-        if sys.argv[2] == 'stats':
-            stats = Feed.stats()
-            if len(sys.argv) > 3:
-                with open(sys.argv[3], 'w') as out:
-                    import json
-                    out.write(json.dumps(stats))
-        elif sys.argv[2] == 'generate':
-            stats = Feed.stats(False)
-            n = None
-
-            if len(sys.argv) > 3:
-                n = int(sys.argv[3])
-            
-            if not n is None:
-                nStats = stats[str(n) + 'gram']
-                words = ['<START>'] * (n - 1)
-    
-                curr = pickWord(nStats, words)
-
-                while curr != '<END>':
-                    words.append(curr)
-                    curr = pickWord(nStats, words[len(words) - (n - 1):])
-
-                words = words[n-1:]
-
-            else:
-                words = ['<START>'] * 4
-
-                curr = pickWordMulti(stats, words, 5)
-                while curr != '<END>':
-                    words.append(curr)
-                    curr = pickWordMulti(stats, words[len(words) - 4:], 5)
-
-                words = words[4:]
-
-            print ' '.join(map(lambda s: s.replace('<period>', '.').replace('<dollar>', '$'), words))
--- randblog/admin/__main__.py
+++ randblog/admin/__main__.py
@@ -5,84 +5,9 @@ from randblog.admin import *
 if __name__ == '__main__':
     print sys.argv[1:]
     if sys.argv[1] == 'feed':
-        if sys.argv[2] == 'add':
-            Feed.add(sys.argv[3], sys.argv[4])
-        elif sys.argv[2] == 'update':
-            if len(sys.argv) > 3 and sys.argv[3] == 'False':
-                update = False
-            else:
-                update = True
-
-            feeds = Feed.find()
-            do_task_for_feeds(lambda f: f.update(), feeds)
-        elif sys.argv[2] == 'clean_actions':
-            if sys.argv[3] == 'show':
-                if len(sys.argv) > 4:
-                    feeds = [Feed.get(sys.argv[4])]
-                else:
-                    feeds = Feed.find()
-                for feed in feeds:
-                    print feed.name
-                    if 'clean_actions' in feed.info:
-                        for action in feed.info['clean_actions']:
-                            print ' -', action['task'], action['selector']
-
-            elif sys.argv[3] == 'add':
-                f = Feed.get(sys.argv[4])
-                if not 'clean_actions' in f.info:
-                    f.info['clean_actions'] = []
-                f.info['clean_actions'].append({'selector': sys.argv[5], 'task': sys.argv[6]})
-                f.save()
+        feed_command(sys.argv[2:])
     elif sys.argv[1] == 'entry':
-        if sys.argv[2] == 'clean':
-            if len(sys.argv) == 3:
-                toClean = Feed.find()
-            else:
-                toClean = [Feed.get(f) for f in sys.argv[3:]]
-            
-            do_task_for_feeds(lambda f: f.clean(), toClean)
-
-        elif sys.argv[2] == 'stats':
-            if len(sys.argv) == 3:
-                toStat = Feed.find()
-            else:
-                toStat = [Feed.get(f) for f in sys.argv[3:]]
-
-            do_task_for_feeds(lambda f: f.stats_collect(True), toStat)
+        entry_command(sys.argv[2:])
     elif sys.argv[1] == 'global':
-        if sys.argv[2] == 'stats':
-            stats = Feed.stats()
-            if len(sys.argv) > 3:
-                with open(sys.argv[3], 'w') as out:
-                    import json
-                    out.write(json.dumps(stats))
-        elif sys.argv[2] == 'generate':
-            stats = Feed.stats(False)
-            n = None
+        global_command(sys.argv[2:])
 
-            if len(sys.argv) > 3:
-                n = int(sys.argv[3])
-            
-            if not n is None:
-                nStats = stats[str(n) + 'gram']
-                words = ['<START>'] * (n - 1)
-    
-                curr = pickWord(nStats, words)
-
-                while curr != '<END>':
-                    words.append(curr)
-                    curr = pickWord(nStats, words[len(words) - (n - 1):])
-
-                words = words[n-1:]
-
-            else:
-                words = ['<START>'] * 4
-
-                curr = pickWordMulti(stats, words, 5)
-                while curr != '<END>':
-                    words.append(curr)
-                    curr = pickWordMulti(stats, words[len(words) - 4:], 5)
-
-                words = words[4:]
-
-            print ' '.join(map(lambda s: s.replace('<period>', '.').replace('<dollar>', '$'), words))
