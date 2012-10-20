33517f0ed81782ae41c5d05a159695c786589e08
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2012-10-20 13:54:52 -0600

    Add rails admin app

---
+++ admin_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdminApp::Application.config.secret_token = '1d61fcfc0fcd5a9ed6ca2b9c2b1577dd70b812d88bf189976d8851fb6673a5e43677c9faab75b539842ede7e403c66340c2bf66f17b2c1e4b0e14f150dad26ed'
