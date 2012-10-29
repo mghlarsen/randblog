2864eb1b6e7cad24f39e52ab7f90e798a96ca3e5
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2012-10-29 10:03:33 -0600

    More adding of admin app

--- admin_app/config/initializers/secret_token.rb
+++ admin_app/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AdminApp::Application.config.secret_token = '1d61fcfc0fcd5a9ed6ca2b9c2b1577dd70b812d88bf189976d8851fb6673a5e43677c9faab75b539842ede7e403c66340c2bf66f17b2c1e4b0e14f150dad26ed'
+AdminApp::Application.config.secret_token = 'c8c4a533389dda464ad1244824830f7033232af936f79b4b84421458dd19fea2ce3a5014043e1ee9a12696994553714eaf00d3e59f82e8ca723900d48e8fe6f5'
