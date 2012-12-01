6c6342ec29ebd20d80b4cb7d880133e91319edd5
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2012-11-30 17:24:01 -0700

    Refactor & Cleanup on Admin App UI.

--- admin_app/app/helpers/application_helper.rb
+++ admin_app/app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def info_row(name, value)
+    render partial: 'shared/info_row', locals: {name: name, value: value}
+  end
 end
--- admin_app/app/models/entry.rb
+++ admin_app/app/models/entry.rb
@@ -4,6 +4,14 @@ class Entry
   field :feed_name, type: String
   field :links, type: Array
   field :id, type: String
+  field :author, type: String
 
   belongs_to :feed, foreign_key: :feed
+  belongs_to :corpus_item, foreign_key: :corpus_item
+
+  def filter_attributes(excluded_fields)
+    attributes.reject do |key, value|
+      excluded_fields.include?(key)
+    end
+  end
 end
