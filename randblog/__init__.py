65de4dc15c97092e7f146894df7364fcdb0e6abf
Michael G. H. Larsen <mike.gh.larsen@gmail.com>
2013-02-12 08:42:33 -0700

    Fix import error for generator (still have to set up old db connection)

--- randblog/__init__.py
+++ randblog/__init__.py
@@ -1,5 +1,8 @@
 from gevent import monkey; monkey.patch_all()
+from pymongo import Connection
 from mongoengine import connect
 
 MONGO_DB_NAME = 'trgtb-DEV'
 connect(MONGO_DB_NAME)
+
+db = Connection()[MONGO_DB_NAME]
